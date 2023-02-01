e89efb6e5bbe944ed9597973a7122927e438fcd0
Ioanis Oliveira <ioanins0511@gmail.com>
2023-01-31 21:42:06 -0300

    Desafio JavaScript 2

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+function mensagem() {
+console.log("Desafio JavaScript 2 em busca do site perfeito")
+}
+mensagem()
+
+function nome(ReiDoBasquete) {
+    console.log(ReiDoBasquete)
+    
+}
+nome("ioanis")
+
+function qualidades(nome,idade,musica) {
+    console.log(nome,idade,musica)
+}
+qualidades("ioanis", 20, "samba")
+
+function Interesses(Filme,musiquinha) {
+    console.log(Filme,musiquinha)
+}
+Interesses("Poderoso Chefão","Mulher não manda em homem" )
+
+function tabuada(valor) {
+    console.log(valor*3)
+}
+
+tabuada(150)
+tabuada(250)
+tabuada(350)
+
+
+let numero = true
+
+function verdadeiro() {
+   
+if (numero==true) {
+    console.log(numero)
+}
+
+}
+verdadeiro()
